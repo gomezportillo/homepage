0d1239811073460aa75c8e70015b5fc40a0ebcef
Pedro Manuel Gómez-Portillo <pedroma.almagro@gmail.com>
2019-11-17 01:00:33 +0100

    Added new games tab and removed school links and unused gif files

--- js/index.js
+++ js/index.js
@@ -1,10 +1,6 @@
 $( function()
 {
-	//part 1
-	//on click of an an image that provides links
-	//images and divs that should do this
-	//chan, music, school, work
-
+	
 	$('#chan').mouseover( function()
 	{
 		if($('.content').hasClass('active'))
@@ -23,13 +19,13 @@ $( function()
 		$('#musicList').addClass('active').show('300');
 	});
 
-	$('#school').mouseover( function()
+	$('#games').mouseover( function()
 	{
 		if($('.content').hasClass('active'))
 		{
 			$('.content').removeClass('active').hide();
 		}
-		$('#schoolList').addClass('active').show('300');
+		$('#gamesList').addClass('active').show('300');
 	});
 
 	$('#work').mouseover( function()
