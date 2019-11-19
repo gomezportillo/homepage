db6c3ac8c6f1f167e27203caca3105b2f0be0eea
Pedro Manuel Gómez-Portillo <pedroma.almagro@gmail.com>
2019-11-19 23:40:11 +0100

    Checking if the current element is already active before activing it

--- js/index.js
+++ js/index.js
@@ -11,45 +11,60 @@ $( function()
 {
 	$('#blogs').mouseover( function()
 	{
-		removeAllActives();
+		if (!$('#blogIcon').hasClass('active'))
+		{
+			removeAllActives();
 
-		$('#blogList').addClass('active').show('300');
-		$('#blogIcon').addClass('active');
+			$('#blogList').addClass('active').show('300');
+			$('#blogIcon').addClass('active');
+		}
 	});
 
 	$('#music').mouseover( function()
 	{
-		removeAllActives();
+		if (!$('#musicIcon').hasClass('active'))
+		{
+			removeAllActives();
 
-		$('#musicList').addClass('active').show('300');
-		$('#musicIcon').addClass('active');
+			$('#musicList').addClass('active').show('300');
+			$('#musicIcon').addClass('active');
+		}
 	});
 
 	$('#games').mouseover( function()
 	{
-		removeAllActives();
+		if (!$('#gamesIcon').hasClass('active'))
+		{
+			removeAllActives();
 
-		$('#gamesList').addClass('active').show('300');
-		$('#gamesIcon').addClass('active');
+			$('#gamesList').addClass('active').show('300');
+			$('#gamesIcon').addClass('active');
 
-		$('#gamesIcon').attr("src", "images/icons/gameboy.gif");
+			$('#gamesIcon').attr("src", "images/icons/gameboy.gif");
+		}
 	});
 
 	$('#work').mouseover( function()
 	{
-		removeAllActives();
+		if (!$('#workIcon').hasClass('active'))
+		{
+			removeAllActives();
 
-		$('#workList').addClass('active').show('300');
-		$('#workIcon').addClass('active');
+			$('#workList').addClass('active').show('300');
+			$('#workIcon').addClass('active');
 
-		$('#workIcon').attr("src", "images/icons/darksouls.gif");
+			$('#workIcon').attr("src", "images/icons/darksouls.gif");
+		}
 	});
 
 	$('#resources').mouseover( function()
 	{
-		removeAllActives();
+		if (!$('#resourceIcon').hasClass('active'))
+		{
+			removeAllActives();
 
-		$('#resourceList').addClass('active').show('300');
-		$('#resourceIcon').addClass('active');
+			$('#resourceList').addClass('active').show('300');
+			$('#resourceIcon').addClass('active');
+		}
 	});
 });
